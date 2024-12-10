c01d2d3fd51dbb1ce6da72ad7916e61bd6172aad
Jordan Harband <ljharb@gmail.com>
2024-12-10 12:51:43 -0800

    [New] add `channel.delete`

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,7 @@ declare namespace getSideChannel {
 		has: (key: K) => boolean;
 		get: (key: K) => V | undefined;
 		set: (key: K, value: V) => void;
+		delete: (key: K) => boolean;
 	}
 }
 
--- index.js
+++ index.js
@@ -16,57 +16,81 @@ var $weakMapGet = callBound('WeakMap.prototype.get', true);
 var $weakMapSet = callBound('WeakMap.prototype.set', true);
 /** @type {CallBind<typeof WeakMap.prototype.has>} */
 var $weakMapHas = callBound('WeakMap.prototype.has', true);
+/** @type {CallBind<typeof WeakMap.prototype.delete>} */
+var $weakMapDelete = callBound('WeakMap.prototype.delete', true);
 /** @type {CallBind<typeof Map.prototype.get>} */
 var $mapGet = callBound('Map.prototype.get', true);
 /** @type {CallBind<typeof Map.prototype.set>} */
 var $mapSet = callBound('Map.prototype.set', true);
 /** @type {CallBind<typeof Map.prototype.has>} */
 var $mapHas = callBound('Map.prototype.has', true);
+/** @type {CallBind<typeof Map.prototype.delete>} */
+var $mapDelete = callBound('Map.prototype.delete', true);
 
 /*
 * This function traverses the list returning the node corresponding to the given key.
 *
-* That node is also moved to the head of the list, so that if it's accessed again we don't need to traverse the whole list. By doing so, all the recently used nodes can be accessed relatively quickly.
+* That node is also moved to the head of the list, so that if it's accessed again we don't need to traverse the whole list.
+* By doing so, all the recently used nodes can be accessed relatively quickly.
 */
 /** @type {import('./list.d.ts').listGetNode} */
-var listGetNode = function (list, key) { // eslint-disable-line consistent-return
+// eslint-disable-next-line consistent-return
+var listGetNode = function (list, key, isDelete) {
 	/** @type {typeof list | NonNullable<(typeof list)['next']>} */
 	var prev = list;
 	/** @type {(typeof list)['next']} */
 	var curr;
-	for (; (curr = prev.next) !== null; prev = curr) {
+	// eslint-disable-next-line eqeqeq
+	for (; (curr = prev.next) != null; prev = curr) {
 		if (curr.key === key) {
 			prev.next = curr.next;
-			// eslint-disable-next-line no-extra-parens
-			curr.next = /** @type {NonNullable<typeof list.next>} */ (list.next);
-			list.next = curr; // eslint-disable-line no-param-reassign
+			if (!isDelete) {
+				// eslint-disable-next-line no-extra-parens
+				curr.next = /** @type {NonNullable<typeof list.next>} */ (list.next);
+				list.next = curr; // eslint-disable-line no-param-reassign
+			}
 			return curr;
 		}
 	}
 };
 
 /** @type {import('./list.d.ts').listGet} */
+// eslint-disable-next-line consistent-return
 var listGet = function (objects, key) {
-	var node = listGetNode(objects, key);
-	return node && node.value;
+	if (objects) {
+		var node = listGetNode(objects, key);
+		return node && node.value;
+	}
 };
 /** @type {import('./list.d.ts').listSet} */
 var listSet = function (objects, key, value) {
-	var node = listGetNode(objects, key);
-	if (node) {
-		node.value = value;
-	} else {
+	if (objects) {
+		var node = listGetNode(objects, key);
+		if (node) {
+			node.value = value;
+		} else {
 		// Prepend the new node to the beginning of the list
-		objects.next = /** @type {import('./list.d.ts').ListNode<typeof value, typeof key>} */ ({ // eslint-disable-line no-param-reassign, no-extra-parens
-			key: key,
-			next: objects.next,
-			value: value
-		});
+			objects.next = /** @type {import('./list.d.ts').ListNode<typeof value, typeof key>} */ ({ // eslint-disable-line no-param-reassign, no-extra-parens
+				key: key,
+				next: objects.next,
+				value: value
+			});
+		}
 	}
 };
 /** @type {import('./list.d.ts').listHas} */
 var listHas = function (objects, key) {
+	if (!objects) {
+		return false;
+	}
 	return !!listGetNode(objects, key);
+};
+/** @type {import('./list.d.ts').listDelete} */
+// eslint-disable-next-line consistent-return
+var listDelete = function (objects, key) {
+	if (objects) {
+		return listGetNode(objects, key, true);
+	}
 };
 
 /** @type {import('.')} */
@@ -86,6 +110,27 @@ module.exports = function getSideChannel() {
 				throw new $TypeError('Side channel does not contain ' + inspect(key));
 			}
 		},
+		'delete': function (key) {
+			if ($WeakMap && key && (typeof key === 'object' || typeof key === 'function')) {
+				if ($wm) {
+					return $weakMapDelete($wm, key);
+				}
+			} else if ($Map) {
+				if ($m) {
+					return $mapDelete($m, key);
+				}
+			} else {
+				if ($o) { // eslint-disable-line no-lonely-if
+					var root = $o && $o.next;
+					var deletedNode = listDelete($o, key);
+					if (deletedNode && root && root === deletedNode) {
+						$o = void undefined;
+					}
+					return !!deletedNode;
+				}
+			}
+			return false;
+		},
 		get: function (key) { // eslint-disable-line consistent-return
 			if ($WeakMap && key && (typeof key === 'object' || typeof key === 'function')) {
 				if ($wm) {
@@ -96,9 +141,7 @@ module.exports = function getSideChannel() {
 					return $mapGet($m, key);
 				}
 			} else {
-				if ($o) { // eslint-disable-line no-lonely-if
-					return listGet($o, key);
-				}
+				return listGet($o, key);
 			}
 		},
 		has: function (key) {
@@ -111,9 +154,7 @@ module.exports = function getSideChannel() {
 					return $mapHas($m, key);
 				}
 			} else {
-				if ($o) { // eslint-disable-line no-lonely-if
-					return listHas($o, key);
-				}
+				return listHas($o, key);
 			}
 			return false;
 		},
@@ -131,9 +172,10 @@ module.exports = function getSideChannel() {
 			} else {
 				if (!$o) {
 					// Initialize the linked list as an empty node, so that we don't have to special-case handling of the first node: we can always refer to it as (previous node).next, instead of something like (list).head
-					$o = { key: {}, next: null };
+					$o = { next: void undefined };
 				}
-				listSet($o, key, value);
+				// eslint-disable-next-line no-extra-parens
+				listSet(/** @type {NonNullable<typeof $o>} */ ($o), key, value);
 			}
 		}
 	};
